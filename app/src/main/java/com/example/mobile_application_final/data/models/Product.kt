8af007d8e6fcb167e0668e5d68bdfc41ee87419b
Coblodyte <jacobbusiness04@outlook.com>
2025-12-06 15:22:33 -0500

    Test

--- app/src/main/java/com/example/mobile_application_final/data/models/Product.kt
+++ app/src/main/java/com/example/mobile_application_final/data/models/Product.kt
@@ -7,6 +7,7 @@ data class Product (
     val id: String = "",
     val name: String = "",
     val description: String = "",
+
     val price: Double = 0.0,
     val rating: Double = 0.0,
     val category: String = "",
