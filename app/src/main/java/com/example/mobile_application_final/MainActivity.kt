233a3fdc8b24565ff544f04c88a5bf1afce7e7e4
Josh <hi@jossh.dev>
2025-12-07 16:23:41 -0500

    Use material theme builder theme

--- app/src/main/java/com/example/mobile_application_final/MainActivity.kt
+++ app/src/main/java/com/example/mobile_application_final/MainActivity.kt
@@ -8,14 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.mobile_application_final.screens.navigation.AppNavigation
-import com.example.mobile_application_final.ui.theme.MobileapplicationfinalTheme
+import com.example.mobile_application_final.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MobileapplicationfinalTheme {
+            AppTheme {
                 // Remove any Scaffold here. It is handled inside AppNavigation.
                 Surface(modifier = Modifier.fillMaxSize()) {
                     AppNavigation()
