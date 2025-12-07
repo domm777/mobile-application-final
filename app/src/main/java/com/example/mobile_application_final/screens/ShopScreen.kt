88078562f7ae3b14bb048e331132560f2b807d91
Coblodyte <jacobbusiness04@outlook.com>
2025-12-06 20:05:18 -0500

    Removed all un used

--- app/src/main/java/com/example/mobile_application_final/screens/ShopScreen.kt
+++ app/src/main/java/com/example/mobile_application_final/screens/ShopScreen.kt
@@ -24,13 +24,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mobile_application_final.components.HProductCard
 import com.example.mobile_application_final.components.VProductCard
 import com.example.mobile_application_final.data.viewModels.ShopScreenViewModel
-import com.example.mobile_application_final.ui.theme.MobileapplicationfinalTheme
 
 @Composable
 fun ShopScreen() {
